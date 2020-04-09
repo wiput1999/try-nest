d37aa48c0330e57ec15ee9189ef78fcc68e1e40d
Wiput Pootong <wiput.pootong@gmail.com>
2020-04-09 22:03:40 +0700

    :sparkles: add get user by id

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Param,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common'
 import { UsersService } from './users.service'
 import { User } from './user.model'
 
-@Controller()
+@Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get('/users')
+  @Get()
   getAllUsers(): Promise<User[]> {
     return this.usersService.findAll()
+  }
+
+  @Get(':id')
+  async getUserById(@Param() params): Promise<User> {
+    const user = await this.usersService.findOne(params.id)
+
+    if (user) return user
+    throw new HttpException('Not Found', HttpStatus.NOT_FOUND)
   }
 }
